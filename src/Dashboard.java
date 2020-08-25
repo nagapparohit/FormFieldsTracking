da0ca8048b0875904c39a18b00b5eed19f70a7ed
rohit nagappa <rnagappa35@gmail.com>
2020-08-25 12:31:03 +0530

    inital commit

---
+++ src/Dashboard.java
@@ -0,0 +1,54 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Dashboard
+ */
+@WebServlet("/Dashboard")
+public class Dashboard extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Dashboard() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>");
+		
+		String userid = request.getParameter("userid");
+		
+		if(userid==null) {
+			out.println("<h1>No user id was found in hidden form field</h1><br><br>");
+		}else {
+			out.println("<h1>User id found in hidden form field : "+userid+"</h1> <br>");
+		}
+		
+		out.println("</body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
